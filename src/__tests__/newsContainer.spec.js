49792e839083d0b1544f71749dd94bcf7b867fe3
Ivan <goncharov1995.2@gmail.com>
2020-07-01 13:12:03 +0300

    ...

--- src/__tests__/newsContainer.spec.js
+++ src/__tests__/newsContainer.spec.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { shallow, mount } from 'enzyme'
+import { shallow } from 'enzyme'
 import { NewsContainer } from '../containers/NewsContainer'
 
 describe('News container', () => {
@@ -21,6 +21,33 @@ describe('News container', () => {
     })
   })
 
+  describe('News container render <News>', () => {
+    const nextProps = {
+      ...props,
+      data: [{}, {}, {}]
+    }
+
+    const newsContainer = shallow(<NewsContainer {...nextProps} />)
+
+    it('renders properly', () => {
+      expect(newsContainer).toMatchSnapshot()
+    })
+  })
+
+  describe('News container with error', () => {
+    const nextProps = {
+      ...props,
+      msg: 'Something going wrong',
+    }
+
+    const newsContainer = shallow(<NewsContainer {...nextProps} />)
+
+    it('renders properly', () => {
+      expect(newsContainer).toMatchSnapshot()
+    })
+  })
+
+
   describe('News container loading', () => {
     const nextProps = {
       ...props,
@@ -28,67 +55,10 @@ describe('News container', () => {
     }
 
     const newsContainer = shallow(<NewsContainer {...nextProps} />)
-
-    it('render preloader', () => {
-      expect(newsContainer.find('LoadingFullScreen')).toHaveLength(1)
-    })
-
     it('renders properly', () => {
       expect(newsContainer).toMatchSnapshot()
     })
   })
-
-  describe('News container render <News>', () => {
-    const nextProps = {
-      ...props,
-      data: [1, 2, 3]
-    }
-
-    const newsContainer = shallow(<NewsContainer {...nextProps} />)
-
-    it('render <News>', () => {
-      expect(newsContainer.find('News')).toHaveLength(1)
-    })
-
-    // it('renders properly', () => {
-    //   expect(newsContainer).toMatchSnapshot()
-    // })
-  })
-
-  describe('News container with error', () => {
-    const nextProps = {
-      ...props,
-      msg: 'Something going wrong'
-    }
-
-    const newsContainer = mount(<NewsContainer {...nextProps} />)
-
-    it('render message', () => {
-      expect(newsContainer.find('h3').text()).toEqual(nextProps.message)
-    })
-
-    // it('renders properly', () => {
-    //   expect(newsContainer).toMatchSnapshot()
-    // })
-  })
-
-  describe('News container init', () => {
-
-    const mockFetchGetNews = jest.fn()
-    const nextProps = {
-      ...props,
-      handlerRequest: mockFetchGetNews,
-    }
-
-    const newsContainer = mount(<NewsContainer {...nextProps} />)
-
-    // it('renders properly', () => {
-    //   expect(newsContainer).toMatchSnapshot()
-    // })
-
-    it('dispatches the handlerRequest method it receives from props', () => {
-
-      expect(mockFetchGetNews).toHaveBeenCalled()
-    })
-  })
 })
+
+
